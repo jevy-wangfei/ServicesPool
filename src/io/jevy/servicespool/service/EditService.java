19c26b514f30e2d429341bd88fcf7de02602ec37
jevy-wangfei <jvl.jevy@gmail.com>
2014-07-26 20:56:10 +1000

    Create basic classes

---
+++ src/io/jevy/servicespool/service/EditService.java
@@ -0,0 +1,14 @@
+package io.jevy.servicespool.service;
+
+
+public class EditService {
+
+	private EditService() {
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
